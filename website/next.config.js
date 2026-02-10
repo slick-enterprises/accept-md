947415e872654ff589d901fb901a798630a00c82
Hemanth Valsaraj <hemanthvalsaraj@gmail.com>
2026-02-11 04:36:31 +0530

    Website Changes

--- website/next.config.js
+++ website/next.config.js
@@ -1,5 +1,23 @@
 /** @type { import('next').NextConfig } */
 const nextConfig = {
   reactStrictMode: true,
+
+  rewrites: async () => {
+    return {
+      beforeFiles: [
+    {
+      source: '/:path*',
+      destination: '/api/accept-md?path=:path*',
+      has: [
+        {
+          type: 'header',
+          key: 'accept',
+          value: '(.*)text/markdown(.*)',
+        },
+      ],
+    }
+      ],
+    };
+  },
 };
 module.exports = nextConfig;
